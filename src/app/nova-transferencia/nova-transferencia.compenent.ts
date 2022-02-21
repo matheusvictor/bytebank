54f56579239957c21f7768f67425dec0b574b8a9
Matheus Victor <matheusvictor.salles@gmail.com>
2022-02-21 14:13:30 -0300

    Implementa captura das informações do formulário

--- src/app/nova-transferencia/nova-transferencia.compenent.ts
+++ src/app/nova-transferencia/nova-transferencia.compenent.ts
@@ -7,7 +7,14 @@ import { Component } from '@angular/core';
 })
 
 export class NovaTransferenciaComponent{
+
+  valor: number;
+  destino: number;
+
   transferir(){
     console.log('Nova transferência solicitada!');
+    console.log('Valor: ', this.valor);
+    console.log('Destino: ', this.destino);
   }
+
 }
